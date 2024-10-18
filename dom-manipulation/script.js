ced07460e77a379d3cb69dc5482948e3398f400c
alxsoftware2024 <ashenafin2024@gmail.com>
2024-10-18 02:26:55 -0700

    Show randem quote

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -3,3 +3,9 @@ const quotes = [
    
     // more quotes...
   ];
+  function showRandomQuote() {
+    const newQuotes=document.getElementById('newQuoteText').value;
+    document.getElementById('quoteDisplay').innerHTML=newQuotes;
+}
+const newQuote=document.getElementById('newQuote');
+newQuote.addEventListener('click',showRandomQuote);
